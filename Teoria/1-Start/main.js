8d5142972f001b57806d67ab04898f73d324580e
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2020-04-14 00:14:39 +0200

    Learning, what is Instructions in JS

--- Teoria/1-Start/main.js
+++ Teoria/1-Start/main.js
@@ -15,6 +15,25 @@ słowo kluczowe let, nazwa
 
 zmienna => slowo kluczowe, nazwa, znak =, wartość
 let result = 2 + 2;
+
+znak = nie oznacza że jest równe 4, tylko
+przypisz to co jest po prawej stronie do tego co jest po lewej stronie
+wynik  
 */
 
 let result = 2 + 4;
+
+//wykonanie metody - zrób coś z jakimiś danymi
+'tekst'.toUpperCase();
+console.log('czarek'.toUpperCase());
+
+/* 
+INSTRUKCJA:
+- instrukcja może coś zwracać, ale nie tzn:
+    * let result = 'sss';  - zmienna nic nie zwraca, tylko jest zadeklarowana
+        // w konsoli będzie widoczne jako undefined
+    * result lub console.log(result); zwróci już wartość 'sss';
+
+- średniki są obowiązkowe! jeśli nie damy, to interpreter niejawnie go nam doda
+- instrukcje są wykonywane z góry na dół - tak jest czytany program
+*/
