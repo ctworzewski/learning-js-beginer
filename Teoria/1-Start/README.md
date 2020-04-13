95ee9949cf960c298772157756964c8fca940f93
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2020-04-13 23:36:32 +0200

    Add to console object in browser

--- Teoria/1-Start/README.md
+++ Teoria/1-Start/README.md
@@ -2,3 +2,7 @@ importujemy nasz plik js do html'a, po to abysmy mogli go w naszej przeglądarce
 
 cały dokumeny html do body został już wyrenderowany przez przeglądarkę, czyli został stworzony DOM
 
+`console` jest to obiekt 
+`console.log` - metoda log w obiekcie console
+
+- główny obiekt w przeglądarce nazywa się window
--- Teoria/1-Start/main.js
+++ Teoria/1-Start/main.js
@@ -1,1 +1,20 @@
 console.log('Start');
+
+// instrukcja - najmniejsza składowa programu
+// weż wartość z lewej strony i dodaj wartość z prawejk strony
+
+2 + 2
+
+console.log(2 + 2); // wynik wyrażenia
+console.log('2' + '2'); // '2' jako tekst, operator + nie dodaje tylko łączy, będzie to nie typ number, tylko zwykły String
+
+//tworzymy zmienną poprzez jej deklarację
+/*
+zmienną tworzymy poprzez jej deklarację
+słowo kluczowe let, nazwa 
+
+zmienna => slowo kluczowe, nazwa, znak =, wartość
+let result = 2 + 2;
+*/
+
+let result = 2 + 4;
